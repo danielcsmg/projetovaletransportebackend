7034a866436be338d1de84d1b403d2e6a698a60c
danielcsmg <daniel.csouzamg@gmail.com>
2023-02-28 16:15:53 -0300

    First commit

---
+++ src/main/java/com/daniel/valetransporte/ValeTransporteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.valetransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValeTransporteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValeTransporteProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/valetransporte/ValeTransporteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.valetransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValeTransporteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
